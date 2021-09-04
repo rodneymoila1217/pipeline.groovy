f97732bbdc70822382f01fef658b7135e2f1d0f8
rodneymoila1217 <rodneymoila@gmail.com>
2021-09-05 01:00:58 +0200

    added groovy

---
+++ test_pipe.groovy
@@ -0,0 +1,16 @@
+node{
+  stage('Prep') {
+    git 'https://github.com/rodneymoila1217/pipeline.groovy.git'
+   }
+ stage('Build')
+   {
+     sh 'echo "Hello world"'
+   
+   }
+
+ stage()
+  {
+   sh 'Results"
+  }
+
+}
