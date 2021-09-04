91da2f10cccbb998da61de9a6fa3c1269d2635d0
rodneymoila1217 <rodneymoila@gmail.com>
2021-09-05 01:41:29 +0200

    added groovy

--- pipe.groovy
+++ pipe.groovy
@@ -2,6 +2,8 @@ job('Test PipeLine')
 {
   scm {
       git('https://github.com/rodneymoila1217/pipeline.groovy.git'){
+        node / gitConfigName('rodneymoila1217')
+        node / gitConfigEmail('rodneymoila@gmail.com')
 
        }
     }
